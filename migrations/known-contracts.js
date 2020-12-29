d304f0448fddbf305d048ec6446a101993af040b
Frank Wei <frank@frankwei.xyz>
2020-12-29 15:11:04 +0800

    feat: update with unisave router and factory

--- migrations/known-contracts.js
+++ migrations/known-contracts.js
@@ -5,11 +5,15 @@ module.exports = {
     mainnet: '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f',
     ropsten: '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f',
     rinkeby: '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f',
+    bsc_mainnet: '0x32CE36F6eA8d97f9fC19Aab83b9c6D2F52D74470',
+    heco_mainnet: '0x32CE36F6eA8d97f9fC19Aab83b9c6D2F52D74470',
   },
   UniswapV2Router02: {
     mainnet: '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D',
     ropsten: '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D',
     rinkeby: '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D',
+    bsc_mainnet: '0x039b5818e51dfec86c1d56a4668787af0ed1c068',
+    heco_mainnet: '0x039b5818e51dfec86c1d56a4668787af0ed1c068',
   },
   DAI: {
     mainnet: '0x6b175474e89094c44da98b954eedeac495271d0f',

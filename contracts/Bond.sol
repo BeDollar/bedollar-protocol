ac3e4e369b78ad8bc4db7de787cb33a3341af152
Frank Wei <frank@frankwei.xyz>
2021-01-04 11:33:55 +0800

    rebranding

--- contracts/Bond.sol
+++ contracts/Bond.sol
@@ -5,14 +5,14 @@ import '@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol';
 
 contract Bond is ERC20Burnable, Ownable, Operator {
     /**
-     * @notice Constructs the Basis Bond ERC-20 contract.
+     * @notice Constructs the Yield Stable Bond ERC-20 contract.
      */
-    constructor() public ERC20('BAB', 'BAB') {}
+    constructor() public ERC20('YSB', 'YSB') {}
 
     /**
-     * @notice Operator mints basis bonds to a recipient
+     * @notice Operator mints Yield Stable bonds to a recipient
      * @param recipient_ The address of recipient
-     * @param amount_ The amount of basis bonds to mint to
+     * @param amount_ The amount of Yield Stable bonds to mint to
      * @return whether the process has been done
      */
     function mint(address recipient_, uint256 amount_)
--- contracts/Cash.sol
+++ contracts/Cash.sol
@@ -5,10 +5,10 @@ import './owner/Operator.sol';
 
 contract Cash is ERC20Burnable, Operator {
     /**
-     * @notice Constructs the Basis Cash ERC-20 contract.
+     * @notice Constructs the Yield Stable Dollar ERC-20 contract.
      */
-    constructor() public ERC20('BAC', 'BAC') {
-        // Mints 1 Basis Cash to contract creator for initial Uniswap oracle deployment.
+    constructor() public ERC20('YSD', 'YSD') {
+        // Mints 1 Yield Stable Dollar to contract creator for initial Uniswap oracle deployment.
         // Will be burned after oracle deployment
         _mint(msg.sender, 1 * 10**18);
     }
@@ -17,14 +17,14 @@ contract Cash is ERC20Burnable, Operator {
     //        super._beforeTokenTransfer(from, to, amount);
     //        require(
     //            to != operator(),
-    //            "basis.cash: operator as a recipient is not allowed"
+    //            "YSD.cash: operator as a recipient is not allowed"
     //        );
     //    }
 
     /**
-     * @notice Operator mints basis cash to a recipient
+     * @notice Operator mints Yield Stable Dollar to a recipient
      * @param recipient_ The address of recipient
-     * @param amount_ The amount of basis cash to mint to
+     * @param amount_ The amount of Yield Stable Dollar to mint to
      * @return whether the process has been done
      */
     function mint(address recipient_, uint256 amount_)

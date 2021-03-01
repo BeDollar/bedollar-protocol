45fc9c64b241edfbcd66e35337b86b95c6303ad6
Frank Wei <frank@frankwei.xyz>
2021-03-01 15:57:44 +0800

    fix: better script

--- OracleUpdateBot/oracleUpdate.js
+++ OracleUpdateBot/oracleUpdate.js
@@ -1,19 +1,25 @@
 const { ethers } = require('ethers');
 const OracleJSON = require('./ABI/Oracle.json');
 
-const oracleAddress = process.env.ORACLE_ADDRESS;
-const provider = new ethers.providers.JsonRpcProvider(process.env.JSON_RPC_API);
+const [JSON_RPC_API, ORACLE_ADDRESS] = process.argv.slice(2);
+const provider = new ethers.providers.JsonRpcProvider(JSON_RPC_API);
 const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
-const Oracle = new ethers.Contract(oracleAddress, OracleJSON.abi, wallet);
+const Oracle = new ethers.Contract(ORACLE_ADDRESS, OracleJSON.abi, wallet);
 
 async function main() {
     const request = await Oracle.functions.update();
     return request.wait(1);
 }
 
+// Nice way to end a Promise function script
 main()
-  .then(() => process.exit(0))
+  .then((receipt) => {
+    console.info(`Oracle ${ORACLE_ADDRESS} updated at ${new Date().toLocaleString()}`);
+    console.info(`Tx hash: ${receipt.transactionHash}`)
+    process.exit(0);
+  })
   .catch((error) => {
+    console.error(`Error happened when update Oracle ${ORACLE_ADDRESS}: `)
     console.error(error);
     process.exit(1);
   });
